6486dbb503fbaf3ba5844004b3e8abb3dbb14c6a
Nate Holland <nateholland6@gmail.com>
2015-04-01 09:10:17 -0400

    initial file commit

---
+++ contact.php
@@ -0,0 +1,286 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="Portfolio" content="">
+    <meta name="Nate Holland" content="">
+
+    <title>Nate Holland – Designer</title>
+
+    <!-- Bootstrap core CSS -->
+    <link href="css/bootstrap.css" rel="stylesheet">
+    <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css">
+    
+    <!-- Raleway Font -->
+    <link href='http://fonts.googleapis.com/css?family=Raleway:300' rel='stylesheet' type='text/css'>
+        
+    <!-- Stylesheet -->
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+
+  <body>
+    <div id="navbar" class="navbar navbar-inverse"> 
+     <div class="container">   
+       <div class="navbar-header">
+         <a class="navbar-brand" href="index.html"></a>
+         <a class="icon" data-toggle="collapse" data-target="#menu" href="#"><i class="icon-align-justify pull-right"></i></a>
+        <span class="tagline"></span></div>   
+        
+       
+        <div id="menu" class="collapse dropdown">
+          <div class="arrow-up"></div>
+          <ul class="menu">
+            <li><a href="index.html#contact">Contact Me</a></li>
+            <li><a href="http://www.linkedin.com/pub/nate-holland/59/598/378/" target="blank">LinkedIn</a></li>
+            <li><a href="http://www.behance.net/nateholland" target="blank">Behance</a></li>
+          </ul>
+        </div>
+        
+      </div>
+    </div>    
+
+      
+    <div id="home" class="section welcome">
+      
+      <div class="container">
+        <div class="content">
+    			<!-- FILTER CONTROLS -->
+
+    			<div class="controls">	
+            <a href="#" class="filter" data-filter="print">Print & Digital</a>
+  				  <a href="#" class="filter" data-filter="web">Web</a>
+  				  <a href="#" class="filter" data-filter="motion">Motion</a>
+  				  <a href="#" class="filter" data-filter="photo">Photo</a>
+  				  <span class="stretch"></span>
+    			</div>
+
+    		</div>
+    		<div class="gallery row">
+          <!-- Use class "icons" to feature the post -->
+
+          <!-- Print & Digital Category -->
+    		  <ul>
+    		    <li class="col-md-4 col-sm-4 col-xs-12 mix icons print" data-cat="print">
+                <a href="negativland.html" class="mix-cover gray">
+                  <img class="placeholder" src="img/negativland_thumb.jpg" alt="icon clock">
+                  <span class="overlay"><span class="valign"></span><i class="icon-search"></i></span>
+                </a>                
+            </li>
+            <li class="col-md-4 col-sm-4 col-xs-12 mix print" data-cat="print">
+                <a href="super-street.html" class="mix-cover">
+                  <img class="placeholder" src="img/super_street_thumb.jpg" alt="super street">
+                  <div class="overlay"><span class="valign"></span><i class="icon-search"></i></div>
+                </a>
+            </li>
+            <li class="col-md-4 col-sm-4 col-xs-12 mix print" data-cat="print">
+                <a href="icons.html" class="mix-cover">
+                  <img class="placeholder" src="img/dalek_thumb.png" alt="dalek icon">
+                  <div class="overlay"><span class="valign"></span><i class="icon-search"></i></div>
+                </a>
+            </li>
+            <li class="col-md-4 col-sm-4 col-xs-12 mix print" data-cat="print">
+                <a href="methodology-book.html" class="mix-cover">
+                  <img class="placeholder" src="img/method_book_3_thumb.jpg" alt="methodology book">
+                  <div class="overlay"><span class="valign"></span><i class="icon-search"></i></div>
+                </a>
+            </li><li class="col-md-4 col-sm-4 col-xs-12 mix print" data-cat="print">
+                <a href="angry-birds.html" class="mix-cover">
+                  <img class="placeholder" src="img/angry_birds_red_front_thumb.jpg" alt="angry birds">
+                  <div class="overlay"><span class="valign"></span><i class="icon-search"></i></div>
+                </a>
+            </li>
+            <!-- Web Category -->
+            <li class="col-md-4 col-sm-4 col-xs-12 mix web icons" data-cat="web">
+               <a href="jlr.html" class="mix-cover gray">
+                  <img class="placeholder" src="img/jlr_thumb.jpg" alt="julia landauer racing">
+                  <div class="overlay"><span class="valign"></span><i class="icon-search"></i></div>
+                </a>
+            </li>
+            <li class="col-md-4 col-sm-4 col-xs-12 mix web" data-cat="web">
+                <a href="hunter-cattle.html" class="mix-cover gray">
+                  <img class="placeholder" src="img/hunter_cattle_thumb.jpg" alt="hunter cattle">
+                  <div class="overlay"><span class="valign"></span><i class="icon-search"></i></div>
+                </a>
+            </li>
+            <!-- Motion Category -->
+            <li class="col-md-4 col-sm-4 col-xs-12 mix motion icons" data-cat="motion">
+                <a href="demo-reel.html" class="mix-cover">
+                  <img class="placeholder" src="img/demo_thumb.jpg" alt="demo reel">
+                  <div class="overlay"><span class="valign"></span><i class="icon-search"></i></div>
+                </a>
+            </li>
+            <li class="col-md-4 col-sm-4 col-xs-12 mix motion" data-cat="motion">
+                <a href="time-study.html" class="mix-cover">
+                  <img class="placeholder" src="img/time_study_thumb.jpg" alt="study of time">
+                  <div class="overlay"><span class="valign"></span><i class="icon-search"></i></div>
+                </a>
+            </li>
+            <li class="col-md-4 col-sm-4 col-xs-12 mix motion" data-cat="motion">
+                <a href="brooke.html" class="mix-cover">
+                  <img class="placeholder" src="img/brooke_thumb.jpg" alt="brooke terrell logo">
+                  <div class="overlay"><span class="valign"></span><i class="icon-search"></i></div>
+                </a>
+            </li>
+            <li class="col-md-4 col-sm-4 col-xs-12 mix motion" data-cat="motion">
+                <a href="vector-anim.html" class="mix-cover">
+                  <img class="placeholder" src="img/vector_thumb.jpg" alt="vector animation">
+                  <div class="overlay"><span class="valign"></span><i class="icon-search"></i></div>
+                </a>
+            </li>
+            <li class="col-md-4 col-sm-4 col-xs-12 mix motion" data-cat="motion">
+                <a href="coffee-anim.html" class="mix-cover">
+                  <img class="placeholder" src="img/coffee_thumb.jpg" alt="coffee animation">
+                  <div class="overlay"><span class="valign"></span><i class="icon-search"></i></div>
+                </a>
+            </li>
+            <!-- Photo Category -->
+            <li class="col-md-4 col-sm-4 col-xs-12 mix photo" data-cat="photo">
+                <a href="studio-work.html" class="mix-cover">
+                  <img class="placeholder" src="img/studio_thumb.jpg" alt="studio work">
+                  <div class="overlay"><span class="valign"></span><i class="icon-search"></i></div>
+                </a>
+            </li>
+            <li class="col-md-4 col-sm-4 col-xs-12 mix photo" data-cat="photo">
+                <a href="bonaire.html" class="mix-cover">
+                  <img class="placeholder" src="img/bonaire_thumb.jpg" alt="placeholder">
+                  <div class="overlay"><span class="valign"></span><i class="icon-search"></i></div>
+                </a>
+            </li>
+            <li class="col-md-4 col-sm-4 col-xs-12 mix photo" data-cat="photo">
+                <a href="day-night.html" class="mix-cover">
+                  <img class="placeholder" src="img/day_night_broughton_thumb.jpg" alt="placeholder">
+                  <div class="overlay"><span class="valign"></span><i class="icon-search"></i></div>
+                </a>
+            </li>
+      		  
+      		  
+      		  
+    		  </ul>
+        </div>        
+      </div>
+      
+    </div><!-- /.container -->
+    
+    <!-- Services Section -->
+    <div id="services" class="section">
+      <div class="container">
+        
+        <div class="content">
+          
+          <div class="row">
+            <div class="col-md-6 col-sm-6 col-xs-12 pull-right slide-in">
+              <h1>About Me.</h1>
+              <p>Hello! I'm Nate Holland, and I'm a Web, Graphic, & Motion Designer, living in Savannah. I'm a BFA Graphic Design Candidate at Savannah College of Art and Design, and I will be graduating in May 2014. 
+
+<br /><br />When I'm not working on freelance projects, staying up late writing code, or fragging zombies, you can usually find me biking around the city like a mad man, tinkering with my custom built computer, drinking coffee somewhere around town, watching Star Wars, or finding a new mod for my car.<br /><br />
+
+If you want to find out more, check out my resume, or drop me a line below. <br /><br />
+<a href="img/nathaniel-holland-resume.pdf" target="blank"><button class="btn btn-medium"><i class="icon-file"></i>Download Resume</button></a>
+</p>
+            </div>
+            <div class="col-md-6 col-sm-6 col-xs-12 pull-right">
+              <img class="slide-in left" src="img/profile.png" alt="monitor">
+            </div>
+          </div>
+          <div class="row last">
+            <div class="col-md-5 col-sm-5 col-xs-12">
+                <h1>What I can Do.</h1>
+                <h3>• Make Cool Designs</h3>
+                <p>On paper, in motion, or even on the web.</p><br>
+
+               <h3> • Shoot Crisp Photos</h3>
+                 <p>With plain natural light, or in a full studio setting. </p><br>
+
+                  <h3> • Fix Things</h3>
+                 <p>With my extensive experience in mechanics and other types of fabrication and troubleshooting.</p><br>
+
+                  <h3> • Fight Zombies and Aliens</h3>
+                 <p>Those brain munchers and planet destroyers won't stand a chance against my latest hyper-combo</p>
+
+              </div>
+            <div class="col-md-7 col-sm-7 col-xs-12">
+              <img class="slide-in right" src="img/can-do-icon_alt.png" alt="i can do">
+            </div>
+          </div>
+        </div><!-- /.content -->
+      </div>
+    </div>
+    
+    
+    <!-- Contact Section -->
+    <div id="contact" class="section">
+      <div class="container">
+        
+        <div class="content">
+          <h1>Get In Touch</h1>
+
+  <?php
+    $name = $_POST['name'];
+    $email = $_POST['email'];
+    $message = $_POST['message'];
+    $from = 'From: My Website'; 
+    $to = 'hello@nathanielholland.com'; 
+    $subject = 'Hello';
+      
+    $body = "From: $name\n E-Mail: $email\n Message:\n $message";
+        
+    if ( isset($_POST['submit']) ) {    
+    if (mail ($to, $subject, $body, $from)) { 
+        echo '<h3>Your message has been sent!</h3>';
+    } else { 
+        echo '<p>Something went wrong, go back and try again!</p>'; 
+    }
+
+
+    } 
+?>  
+          
+          <form role="form" method="post" action="contact.php">
+            <div class="form-group">
+              <input name="name" type="text" class="form-control" id="exampleInputName" placeholder="Your Name" required>
+              <label for="exampleInputName"><i class="icon-tag"></i></label>
+              <div class="clearfix"></div>
+            </div>
+            <div class="form-group">
+              <input name="email" type="email" class="form-control" id="exampleInputEmail1" placeholder="Your Email" required>
+              <label for="exampleInputEmail1"><i class="icon-inbox"></i></label>
+              <div class="clearfix"></div>
+            </div>
+            <div class="form-group textarea">
+              <textarea name="message" rows="6" class="form-control" id="exampleInputMessage" placeholder="Message" required></textarea>
+              <label for="exampleInputMessage"><i class="icon-pencil"></i></label>
+              <div class="clearfix"></div>
+            </div>
+            
+            <button name="submit" type="submit" value="Submit" class="btn btn-large">Send Message</button>
+          </form>
+          
+        </div>
+      </div>
+    </div><!-- /.container -->    
+    <br />
+    
+    <!-- Google Analytics Tracking
+    ================================================== -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-57521713-1', 'auto');
+      ga('send', 'pageview');
+
+    </script>
+
+    <!-- Bootstrap core JavaScript
+    ================================================== -->
+    <!-- Placed at the end of the document so the pages load faster -->
+    <script src="js/jquery-1.9.1.min.js"></script>
+    <script src="js/jquery.mixitup.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/init.js"></script>
+  </body>
+</html>
